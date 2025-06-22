56621b32cfc71bf2fdae0b910d42a0760b4db093
DevByDipto <dipto.1900@gmail.com>
2025-06-22 16:05:53 +0600

    firebase jwt token practice

--- src/api/applicstionsApi.js
+++ src/api/applicstionsApi.js
@@ -3,7 +3,7 @@ export const myApplicationsPromise = (email,accessToken) => {
   return fetch(`http://localhost:3000/applications?email=${email}`,{
     credentials:'include',
     headers:{
-      authorization:`Beadrer${accessToken}`
+      authorization:`Beadrer ${accessToken}`
     }
   })
   .then((res) => res.json());
